ceb92c3500bcb1f6e1b5d62ccfefcc81b5b4c885
LouisPino <ljpino95@gmail.com>
2026-01-07 16:58:16 -0500

    archive

--- layout.js
+++ layout.js
@@ -1,5 +1,5 @@
 async function loadHeader() {
-    const resp = await fetch("/header.html");
+    const resp = await fetch("./header.html");
     const html = await resp.text();
     document.getElementById("site-header").innerHTML = html;
 }
